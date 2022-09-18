f5f27af2417fde3cb329b906e2b72f6f097b51a6
bewflast <68069182+bewflast@users.noreply.github.com>
2022-09-19 00:00:36 +0300

    косметические правки

--- addons/glua_scan/lua/autorun/server/glua_scan.lua
+++ addons/glua_scan/lua/autorun/server/glua_scan.lua
@@ -1,9 +1,11 @@
 require("CHTTP")
 util.AddNetworkString("flscan_files")
 
-local TableToJSON = util.TableToJSON
-local JSONToTable = util.JSONToTable
-local fileRead = file.Read
+local DISCORD_WARNING_WEBHOOK = ""
+
+local fileRead		= file.Read
+local TableToJSON	= util.TableToJSON
+local JSONToTable	= util.JSONToTable
 
 local function toKeys(tbl)
 
@@ -15,80 +17,81 @@ local function toKeys(tbl)
 
 end
 
-local Discord_warning_webhook = ''
 FCcside_script = [[
-    local fs = {}
-    local stringSplit = string.Split
-    local stringTrim = string.Trim
-    local fileFind = file.Find
+	local fs = {}
+	local stringSplit = string.Split
+	local stringTrim = string.Trim
+	local fileFind = file.Find
 
-    local function getluamenu(patr)
-        local files, dirs = fileFind(patr, "LuaMenu")
-        for k,v in pairs(dirs) do
-            local npath = #stringSplit(patr, "/") < 2 and  v .. "/" .. patr or  stringTrim(patr, "*") .. v .. "/*" 
-            getluamenu(npath)
-        end
-        for k, v in pairs(files) do 
-            fs[#fs + 1] = stringTrim(patr, "*") .. v
-        end
-    end
+	local function getluamenu(patr)
+		local files, dirs = fileFind(patr, "LuaMenu")
+		for k,v in pairs(dirs) do
+			local npath = #stringSplit(patr, "/") < 2 and  v .. "/" .. patr or  stringTrim(patr, "*") .. v .. "/*" 
+			getluamenu(npath)
+		end
+		for k, v in pairs(files) do 
+			fs[#fs + 1] = stringTrim(patr, "*") .. v
+		end
+	end
 
-    getluamenu("*")
-    net.Start("flscan_files")
-        net.WriteString(util.TableToJSON(fs))
-    net.SendToServer()
-    fs = nil
-    print('checked')
+	getluamenu("*")
+	net.Start("flscan_files")
+		net.WriteString(util.TableToJSON(fs))
+	net.SendToServer()
+	fs = nil
 ]]
 
 hook.Add("PlayerInitialSpawn", "SendFileCheck",
-        function(ply)
-            ply:SendLua("WhatTheFCKisThat = [[" .. string.sub(FCcside_script, 1, 200) .. "]]")
-            ply:SendLua("WhatTheFCKisThat = WhatTheFCKisThat .. [[" .. string.sub(FCcside_script, 201, 400) .. "]]")
-            ply:SendLua("WhatTheFCKisThat = WhatTheFCKisThat .. [[" .. string.sub(FCcside_script, 401, 600) .. "]]")
-            ply:SendLua("WhatTheFCKisThat = WhatTheFCKisThat .. [[" .. string.sub(FCcside_script, 601, #FCcside_script) .. "]]")
-            ply:SendLua("local wut = CompileString(WhatTheFCKisThat, 'lalala') wut()") // because client can disable net.Receive
-        end
+		function(ply)
+			ply:SendLua("WhatTheFCKisThat = [[" .. string.sub(FCcside_script, 1, 200) .. "]]")
+			ply:SendLua("WhatTheFCKisThat = WhatTheFCKisThat .. [[" .. string.sub(FCcside_script, 201, 400) .. "]]")
+			ply:SendLua("WhatTheFCKisThat = WhatTheFCKisThat .. [[" .. string.sub(FCcside_script, 401, 600) .. "]]")
+			ply:SendLua("WhatTheFCKisThat = WhatTheFCKisThat .. [[" .. string.sub(FCcside_script, 601, #FCcside_script) .. "]]")
+			ply:SendLua("local wut = CompileString(WhatTheFCKisThat, 'lalala') wut()") // because client can disable net.Receive
+		end
 )
 
 
 local function files_warning(len, ply)
 
-    print("got", ply:Nick())
-    local tablf = net.ReadString()
-    local fs = toKeys(JSONToTable(fileRead("fnative_files.json", "DATA")))
-    local fsstr = ""
+	local tablf = net.ReadString()
+	local fs	= toKeys(JSONToTable(fileRead("fnative_files.json", "DATA")))
+	local fsstr	= ""
 
-    for k,v in pairs(JSONToTable(tablf)) do
-        if !fs[v] then
-            fsstr = fsstr .. "\n" .. v
-        end
-    end
+	for k,v in pairs(JSONToTable(tablf)) do
+		if !fs[v] then
+			fsstr = fsstr .. "\n" .. v
+		end
+	end
 
-    if fsstr == "" then return end
-    local name = ply:Nick()
-    local params = {
-        ['username'] = name,
-        ['embeds'] = { 
+	if fsstr == "" then return end
 
-            {
-                title = "Detected files:",
-                description ="==========" .. "\n[Profile](http://steamcommunity.com/profiles/" .. ply:SteamID64() .. ')\n==========\n' .. fsstr,                      
-                color = 16711680,    
-            } 
-        }
-    }
-    CHTTP({
-        method = 'POST',
-        url = Discord_warning_webhook .. '?wait=true',
-        body = TableToJSON(params),
-        headers = {
-            ["content-Type"] = "application/json",
-            ["accept"] =  "application/json",
-        },
-        type = "application/json; charset=utf-8"
-    })
-    
+	local name = ply:Nick()
+	local params = 
+	{
+		['username']	= name,
+		['embeds']		= 
+		{ 
+			{
+				title		= "Detected files:",
+				description	="==========" .. "\n[Profile](http://steamcommunity.com/profiles/" .. ply:SteamID64() .. ')\n==========\n' .. fsstr,                      
+				color		= 16711680,    
+			} 
+		}
+	}
+
+	CHTTP({
+		method  = 'POST',
+		url		= DISCORD_WARNING_WEBHOOK .. '?wait=true',
+		body	= TableToJSON(params),
+		headers = 
+		{
+			["content-Type"]	= "application/json",
+			["accept"]			=  "application/json",
+		},
+		type	= "application/json; charset=utf-8"
+	})
+	
 end
 
 net.Receive("flscan_files", files_warning)
